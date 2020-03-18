754f3e167ef21b55da196b4e5681bf24468de207
Canareechka <62265278+Canareechka@users.noreply.github.com>
2020-03-18 22:56:43 +0300

    Create mario2.c

---
+++ mario2.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+int height = 0;
+
+while (height < 1 || height > 8)
+{  
+    height = get_int("Height: ");
+}
+
+int counter = 0;
+
+for (int row=0; row<height; row++) 
+{
+    if (counter != height)
+    {
+        for (int spaces = (height-1) - counter; spaces > 0; spaces--)
+        {
+            printf(" ");
+        }
+
+        for (int hashes = 0; hashes <= counter; hashes++)
+        {
+            printf("#");
+        }
+
+        printf("  ");
+
+        for (int hashes = 0; hashes <= counter; hashes++)
+        {
+            printf("#");
+        }
+
+        printf("\n");
+
+        counter++;
+        
+    }
+}     
+       
+}
